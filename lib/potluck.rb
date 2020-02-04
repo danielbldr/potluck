263eb0c1acc89ae78baf7bd018474d3fad12b0da
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-02-03 21:05:51 -0700

    Complete potluck exercise

---
+++ lib/potluck.rb
@@ -0,0 +1,35 @@
+class Potluck
+  attr_reader :date, :dishes
+
+  def initialize(date)
+    @date = date
+    @dishes = []
+  end
+
+  def add_dish(dish)
+    @dishes << dish
+  end
+
+  def get_all_from_category(category)
+    dishes_by_cat = @dishes.find_all {|dish| dish.category == category}
+  end
+
+  def menu
+    category_array = (dishes.map {|dish| dish.category}).uniq
+    menu_hash = Hash.new
+    @dishes.map do |dish|
+      if menu_hash.key?(dish.category) == true
+        menu_hash[dish.category] << dish.name
+      else
+        menu_hash[dish.category] = [dish.name]
+      end
+    end
+    menu_hash.transform_keys! {|key| key.to_s + 's'}
+    menu_hash.transform_keys!(&:to_sym)
+    menu_hash.transform_values!(&:sort)
+  end
+
+  def ratio(category)
+    ((get_all_from_category(category).length.to_f / dishes.length) * 100).round(1)
+  end
+end
---
+++ test/potluck_test.rb
@@ -0,0 +1,93 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/dish'
+require './lib/potluck'
+
+class PotluckTest < Minitest::Test
+  def test_it_exists
+    potluck = Potluck.new("7-13-18")
+    assert_instance_of Potluck, potluck
+  end
+
+  def test_it_has_a_date
+    potluck = Potluck.new("7-13-18")
+    assert_equal "7-13-18", potluck.date
+  end
+
+  def test_it_has_dishes
+    potluck = Potluck.new("7-13-18")
+    assert_equal [], potluck.dishes
+  end
+
+  def test_it_can_add_dishes
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    potluck = Potluck.new("7-13-18")
+    potluck.add_dish(couscous_salad)
+    assert_equal [couscous_salad], potluck.dishes
+    potluck.add_dish(cocktail_meatballs)
+    assert_equal [couscous_salad, cocktail_meatballs], potluck.dishes
+    assert_equal 2, potluck.dishes.length
+  end
+
+  def test_it_can_return_all_dishes_of_certain_category
+    potluck = Potluck.new("7-13-18")
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    roast_pork = Dish.new("Roast Pork", :entre)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    candy_salad = Dish.new("Candy Salad", :dessert)
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(summer_pizza)
+    potluck.add_dish(roast_pork)
+    potluck.add_dish(cocktail_meatballs)
+    potluck.add_dish(candy_salad)
+
+    assert_equal [potluck.dishes[0], potluck.dishes[1]] ,potluck.get_all_from_category(:appetizer)
+    assert_equal potluck.dishes[0], potluck.get_all_from_category(:appetizer).first
+    assert_equal "Couscous Salad", potluck.get_all_from_category(:appetizer).first.name
+  end
+
+  def test_it_has_a_menu
+    potluck = Potluck.new("7-13-18")
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    roast_pork = Dish.new("Roast Pork", :entre)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    candy_salad = Dish.new("Candy Salad", :dessert)
+    bean_dip = Dish.new("Bean Dip", :appetizer)
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(summer_pizza)
+    potluck.add_dish(roast_pork)
+    potluck.add_dish(cocktail_meatballs)
+    potluck.add_dish(candy_salad)
+    potluck.add_dish(bean_dip)
+
+    return_value = {:appetizers=>["Bean Dip", "Couscous Salad", "Summer Pizza"],:entres=>["Cocktail Meatballs", "Roast Pork"],:desserts=>["Candy Salad"]}
+
+    assert_equal return_value, potluck.menu
+    green_beans = Dish.new("Green Beans", :side)
+    potluck.add_dish(green_beans)
+    new_return_value = {:appetizers=>["Bean Dip", "Couscous Salad", "Summer Pizza"],:entres=>["Cocktail Meatballs", "Roast Pork"],:desserts=>["Candy Salad"], :sides=>["Green Beans"]}
+    assert_equal new_return_value, potluck.menu
+  end
+
+  def test_the_ratio_of_category_to_all_items
+    potluck = Potluck.new("7-13-18")
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    roast_pork = Dish.new("Roast Pork", :entre)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    candy_salad = Dish.new("Candy Salad", :dessert)
+    bean_dip = Dish.new("Bean Dip", :appetizer)
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(summer_pizza)
+    potluck.add_dish(roast_pork)
+    potluck.add_dish(cocktail_meatballs)
+    potluck.add_dish(candy_salad)
+    potluck.add_dish(bean_dip)
+
+    assert_equal 50.0, potluck.ratio(:appetizer)
+    assert_equal 33.3, potluck.ratio(:entre)
+  end
+end
